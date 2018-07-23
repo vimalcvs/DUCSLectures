86ebb979c4b4a002e25ae4c37b8c264b4b1911e7
Vimal K. Vishwakarma <40199448+vimalcvs@users.noreply.github.com>
2018-07-23 17:49:27 +0530

    Create Hash_file_maps.java

---
+++ art/Hash_file_maps.java
@@ -0,0 +1,5 @@
+        Hash_file_maps.put("Get Google Play", "https://raw.githubusercontent.com/vimalcvs/DUCSLectures/master/art/1.png");
+        Hash_file_maps.put("Get Google Play", "https://raw.githubusercontent.com/vimalcvs/DUCSLectures/master/art/2.png");
+        Hash_file_maps.put("Get Google Play", "https://raw.githubusercontent.com/vimalcvs/DUCSLectures/master/art/3.png");
+        Hash_file_maps.put("Get Google Play", "https://raw.githubusercontent.com/vimalcvs/DUCSLectures/master/art/4.png");
+        Hash_file_maps.put("Vimal K. Vishwakarma", "https://raw.githubusercontent.com/vimalcvs/DUCSLectures/master/art/5.png");
